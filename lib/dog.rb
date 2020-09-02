1ca191e009fb3db0ea776dc3cf6d70677e19861f
Nathan Steed <steedn@outlook.com>
2020-09-02 05:23:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,10 +21,10 @@ class Dog
     if self.id
      self.update
    else
-     sql = <<-SQL
+     sql = "
        INSERT INTO dogs (name, breed)
        VALUES (?, ?)
-     SQL
+     "
      DB[:conn].execute(sql, self.name, self.breed)
      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
    end
